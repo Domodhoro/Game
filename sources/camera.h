c47fa91ed6e4ce0f3f571f06588f7294d649135c
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2024-03-03 00:33:25 -0300

    Delete sources directory

--- sources/camera.h
+++
@@ -1,9 +0,0 @@
-#ifndef CAMERA_H
-#define CAMERA_H
-
-void update_camera(Game *game) {
-	game->camera_position.x = game->player.dst.x + (PLAYER_WIDTH - WINDOW_WIDTH) / 2;
-    game->camera_position.y = game->player.dst.y + (PLAYER_HEIGHT - WINDOW_HEIGHT) / 2;
-}
-
-#endif
